717a90f42d5e7420ed218f7bf7c5b204b549d3c3
Gurkiran Singh <80485439+gurkiran333@users.noreply.github.com>
2026-09-03 14:40:02 +0530

    Change home route response message

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ def create_app():
     @app.route('/')
     def home():
         print("inside home function")
-        return 'Hurray Sudhanshu hurray1234!'
+        return 'Hello Flask App!!!'
 
     @app.route('/test')
     def test():
